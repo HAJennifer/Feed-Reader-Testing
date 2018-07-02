d5bd2ae45694160321f7df2f9c5b4add2f2d763e
Albert Jennifer <35313442+HAJennifer@users.noreply.github.com>
2018-07-02 14:58:06 +0200

    new test suite named "New Feed Selection"

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -101,8 +101,26 @@ $(function() {
 
     /* TODO: Write a new test suite named "New Feed Selection" */
 
+        describe('New Feed Selection', function() {
+
         /* TODO: Write a test that ensures when a new feed is loaded
          * by the loadFeed function that the content actually changes.
          * Remember, loadFeed() is asynchronous.
          */
+       var oldContent;
+             
+        beforeEach(function(done) {
+            loadFeed(0, function() {
+                oldContent = $('.feed').html();
+                loadFeed(1, function() {
+                    done();
+                });
+            });
+        });
+        it("content changes by new feed", function(done) {
+            var newContent = $('.feed').html();
+            expect(newContent).not.toEqual(oldContent);
+            done();
+        });
+    });
 }());
